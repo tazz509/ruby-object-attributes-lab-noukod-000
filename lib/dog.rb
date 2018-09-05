185e308181675dff9b195be3658ff0731bbaf96d
carly <carly@noukod.com>
2018-09-05 15:06:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog
-  def (name)
+  def(name)
       @name = name
     end
 
@@ -7,7 +7,7 @@ class Dog
       @name
     end
 
-  def (breed)
+  def(breed)
     @breed= breed
   end
 
--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ class Person
   def (name)
     @name = name
   end
-  
+
   def name
     @name
   end
